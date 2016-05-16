82229dd8b1bbdc920959742578b68278341bf70f
SkyBeast <skybeastmc@gmail.com>
2016-05-16 10:40:31 +0200

    base commit

---
+++ InventorySwitcher/src/fr/skybeastmc/hotbarswitcher/Debug.java
@@ -0,0 +1,65 @@
+package fr.skybeastmc.hotbarswitcher;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Debug implements Listener {
+	private static boolean debug;
+
+	public static void debug(Object o) {
+		if(debug) Bukkit.getLogger().info(String.valueOf(o));
+	}
+	public static void bc(Object o) {
+		if(debug) Bukkit.broadcastMessage(String.valueOf(o));
+	}
+	public static boolean isDebug() {
+		return debug;
+	}
+	public static void setDebug(boolean debug) {
+		Debug.debug = debug;
+	}
+	public static void error(Throwable error, String phase, boolean disable) {
+		for(Player player : Bukkit.getOnlinePlayers()) {
+			if(player.isOp()) {
+				player.sendMessage("§c["+Main.getPlugin().getName()+"] Oops, there was an error while phase \""+phase+"\"!");
+				player.sendMessage("§cStacktrace: ");
+				if(debug) player.sendMessage("§c"+stackTraceToString(error));
+				else player.sendMessage("§c[See console]");
+				if(disable) {
+					player.sendMessage("§4The plugin can't work now! Disabling...");
+					Bukkit.getPluginManager().disablePlugin(Main.getPlugin());
+				}
+			}
+		}
+		Bukkit.getLogger().severe("["+Main.getPlugin().getName()+"] Oops, there was an error while phase \""+phase+"\"!");
+		Bukkit.getLogger().severe("Stacktrace: ");
+		Bukkit.getLogger().severe(stackTraceToString(error));
+		if(disable) {
+			Bukkit.getLogger().severe("The plugin can't work now! Disabling...");
+			Bukkit.getPluginManager().disablePlugin(Main.getPlugin());
+		}
+	}
+	private static String stackTraceToString(Throwable error) {
+		StringWriter errors = new StringWriter();
+		error.printStackTrace(new PrintWriter(errors));
+		return errors.toString();
+	}
+	
+	@EventHandler
+	public void onInteractEvent(PlayerInteractEvent event) {
+		try {
+			
+		} catch(Exception e) {
+			error(e, "Debug", false);
+		}
+	}
+	public static void info(Object o) {
+		Bukkit.getLogger().info(String.valueOf(o));
+	}
+}
---
+++ InventorySwitcher/src/fr/skybeastmc/hotbarswitcher/Main.java
@@ -0,0 +1,55 @@
+package fr.skybeastmc.hotbarswitcher;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	private static JavaPlugin plugin;
+	private static YamlConfiguration config;
+	
+	public void onEnable() {
+		try {
+			File f = new File(getDataFolder()+File.separator+"config.yml");
+			f.getParentFile().mkdirs();
+			if(!f.exists()) f.createNewFile();
+			
+			config = YamlConfiguration.loadConfiguration(f);
+			
+		} catch (Exception e) {Debug.error(e, "Config file loading", true);}
+
+		try {
+			plugin = this;
+			
+			boolean debug = config.getBoolean("debug");
+			Debug.setDebug(debug);
+			if(debug) getServer().getPluginManager().registerEvents(new Debug(), this);
+			
+		} catch (Exception e) {Debug.error(e, "Enabling", true);}
+		
+	}
+	
+	public void onDisable() {}
+	
+
+	public static JavaPlugin getPlugin() {
+		return plugin;
+	}
+	
+	public static File getFolder() {
+		return plugin.getDataFolder();
+	}
+
+	public static void setPlugin(JavaPlugin plugin) {
+		Main.plugin = plugin;
+	}
+
+	public static YamlConfiguration getConf() {
+		return config;
+	}
+
+	public static void setConf(YamlConfiguration config) {
+		Main.config = config;
+	}
+}
